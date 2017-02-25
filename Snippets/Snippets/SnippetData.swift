b3244e75afdec3b66866615970953b5919f00fa7
孙立国 <sunliguo@sunliguodemacbook-pro.local>
2017-02-25 10:12:09 +0800

    add new button for creating new snippet data

---
+++ Snippets/Snippets/SnippetData.swift
@@ -0,0 +1,15 @@
+//
+//  SnippetData.swift
+//  Snippets
+//
+//  Created by SunLiguo on 25/02/2017.
+//  Copyright © 2017 孙立国. All rights reserved.
+//
+
+import Foundation
+
+struct SnippetData{
+    init(){
+        print("New Snippet created")
+    }
+}
--- Snippets/Snippets/ViewController.swift
+++ Snippets/Snippets/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
+    var data: [SnippetData] = [SnippetData]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +24,9 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func createNewSnippet(_ sender: AnyObject) {
+        let newSnippet = SnippetData()
+        data.append(newSnippet)
+    }
 }
 
